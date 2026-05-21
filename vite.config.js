526ee453e56e1fb642a91a5ff789222ea65ef664
Aron Rosenfield <aron.rosenfield@gmail.com>
2026-05-21 08:43:50 -0600

    Fix vite config - use esbuild, split companies chunk

--- vite.config.js
+++ vite.config.js
@@ -4,9 +4,13 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   build: {
-    minify: 'terser',
-    terserOptions: {
-      mangle: false
+    minify: 'esbuild',
+    rollupOptions: {
+      output: {
+        manualChunks: {
+          companies: ['./src/companies.js']
+        }
+      }
     }
   }
 })
