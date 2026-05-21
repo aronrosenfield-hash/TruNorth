21daeec756075435e3a18469cb94ca5a7f90784e
Aron Rosenfield <aron.rosenfield@gmail.com>
2026-05-21 08:46:24 -0600

    Final revert - back to stable

--- vite.config.js
+++ vite.config.js
@@ -3,13 +3,4 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  build: {
-    rollupOptions: {
-      output: {
-        manualChunks: (id) => {
-          if (id.includes('companies.js')) return 'companies';
-        }
-      }
-    }
-  }
 })
